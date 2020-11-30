89233615df9e1adff523c1def009d71de910e529
y1jay <67768207+y1jay@users.noreply.github.com>
2020-11-30 22:35:57 +0900

    add

---
+++ D_day/ios/Pods/Target Support Files/Pods-D_day-D_dayTests/Pods-D_day-D_dayTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_D_day_D_dayTests : NSObject
+@end
+@implementation PodsDummy_Pods_D_day_D_dayTests
+@end
---
+++ D_day/ios/Pods/Target Support Files/Pods-D_day-tvOS/Pods-D_day-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_D_day_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_D_day_tvOS
+@end
